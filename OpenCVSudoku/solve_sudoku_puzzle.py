ddd4d69417e252574b8ca709f597e4c247f935a7
thetime50 <1004763777@qq.com>
2020-11-01 19:53:22 +0800

    函数

--- OpenCVSudoku/solve_sudoku_puzzle.py
+++ OpenCVSudoku/solve_sudoku_puzzle.py
@@ -18,85 +18,101 @@ import argparse
 import imutils
 import cv2
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", required=True,
-    help="path to trained digit classifier")
-ap.add_argument("-i", "--image", required=True,
-    help="path to input Sudoku puzzle image")
-ap.add_argument("-d", "--debug", type=int, default=-1,
-    help="whether or not we are visualizing each step of the pipeline")
-args = vars(ap.parse_args())
+
+def solve_sudoku(model,image,debug):
+
+    # find the puzzle in the image and then
+    (puzzleImage, warped) = find_puzzle(image, debug=debug)
+    # initialize our 9x9 Sudoku board
+    board = np.zeros((9, 9), dtype="int") # 9x9 数独矩阵
+
+    stepX = warped.shape[1] // 9
+    stepY = warped.shape[0] // 9
+
+    cellLocs = [] # puzzle cells ROI
+
+    for y in range(0, 9):
+        row = [] # row ROIs
+        for x in range(0, 9):
+            startX = x * stepX
+            startY = y * stepY
+            endX = (x + 1) * stepX
+            endY = (y + 1) * stepY
+            row.append((startX, startY, endX, endY))
+            
+            cell = warped[startY:endY, startX:endX] # 原图裁切出单元格
+            digit = extract_digit(cell, debug=debug) # 是字符单元格
+            # verify that the digit is not empty
+            if digit is not None:
+                roi = cv2.resize(digit, (28, 28))
+                roi = roi.astype("float") / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0) # 从第几个维度的位置插入一个维度 [[item]]
+                # classify the digit and update the Sudoku board with the
+                # prediction
+                # presult = model.predict(roi).argmax(axis=1)) # [[0的期望,1的期望...]]
+                # presult.argmax(axis=1)) # 在第1维做最值运算
+                pred = model.predict(roi).argmax(axis=1)[0] # ocr 识别数字
+                board[y, x] = pred
+        # add the row to our cell locations
+        cellLocs.append(row)
+
+    puzzle = Sudoku(3, 3, board=board.tolist())
+    solution = puzzle.solve() # 解数独
+    return {
+        "puzzle" : puzzle,
+        "solution" : solution,
+        "cellLocs" : cellLocs,
+        "puzzleImage" : puzzleImage
+    }
 
 
-# 加载模型
-print("[INFO] loading digit classifier...")
-model = load_model(args["model"])
-# 加载图片
-print("[INFO] processing image...")
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=600)
+if __name__ == "__main__":
+    
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-m", "--model", required=True,
+        help="path to trained digit classifier")
+    ap.add_argument("-i", "--image", required=True,
+        help="path to input Sudoku puzzle image")
+    ap.add_argument("-d", "--debug", type=int, default=-1,
+        help="whether or not we are visualizing each step of the pipeline")
+    args = vars(ap.parse_args())
 
-# find the puzzle in the image and then
-(puzzleImage, warped) = find_puzzle(image, debug=args["debug"] > 0)
-# initialize our 9x9 Sudoku board
-board = np.zeros((9, 9), dtype="int") # 9x9 数独矩阵
+    # 加载模型
+    print("[INFO] loading digit classifier...")
+    model = load_model(args["model"])
 
-stepX = warped.shape[1] // 9
-stepY = warped.shape[0] // 9
+    # 加载图片
+    print("[INFO] processing image...")
+    image = cv2.imread(args["image"])
+    image = imutils.resize(image, width=600)
 
-cellLocs = [] # puzzle cells ROI
+    solve_result = solve_sudoku(model,image,debug=args["debug"] > 0)
 
-for y in range(0, 9):
-    row = [] # row ROIs
-    for x in range(0, 9):
-        startX = x * stepX
-        startY = y * stepY
-        endX = (x + 1) * stepX
-        endY = (y + 1) * stepY
-        row.append((startX, startY, endX, endY))
-        
-        cell = warped[startY:endY, startX:endX] # 原图裁切出单元格
-        digit = extract_digit(cell, debug=args["debug"] > 0) # 是字符单元格
-        # verify that the digit is not empty
-        if digit is not None:
-            roi = cv2.resize(digit, (28, 28))
-            roi = roi.astype("float") / 255.0
-            roi = img_to_array(roi)
-            roi = np.expand_dims(roi, axis=0) # 从第几个维度的位置插入一个维度 [[item]]
-            # classify the digit and update the Sudoku board with the
-            # prediction
-            # presult = model.predict(roi).argmax(axis=1)) # [[0的期望,1的期望...]]
-            # presult.argmax(axis=1)) # 在第1维做最值运算
-            pred = model.predict(roi).argmax(axis=1)[0] # ocr 识别数字
-            board[y, x] = pred
-    # add the row to our cell locations
-    cellLocs.append(row)
+    puzzle =solve_result["puzzle"] 
+    solution =solve_result["solution"] 
+    cellLocs =solve_result["cellLocs"] 
+    puzzleImage =solve_result["puzzleImage"] 
+    
+    puzzle.show()
+    solution.show_full()
 
-# construct a Sudoku puzzle from the board
-print("[INFO] OCR'd Sudoku board:")
-puzzle = Sudoku(3, 3, board=board.tolist())
-puzzle.show()
-# solve the Sudoku puzzle
-print("[INFO] solving Sudoku puzzle...")
-solution = puzzle.solve() # 解数独
-solution.show_full()
-
-# loop over the cell locations and board
-for (cellRow, boardRow) in zip(cellLocs, solution.board):
-    # loop over individual cell in the row
-    for (box, digit) in zip(cellRow, boardRow):
-        # unpack the cell coordinates
-        startX, startY, endX, endY = box
-        # compute the coordinates of where the digit will be drawn
-        # on the output puzzle image
-        textX = int((endX - startX) * 0.33)
-        textY = int((endY - startY) * -0.2)
-        textX += startX
-        textY += endY
-        # draw the result digit on the Sudoku puzzle image
-        cv2.putText(puzzleImage, str(digit), (textX, textY),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
-# show the output image
-cv2.imshow("Sudoku Result", puzzleImage)
-cv2.waitKey(0)
+    # loop over the cell locations and board
+    for (cellRow, boardRow) in zip(cellLocs, solution.board):
+        # loop over individual cell in the row
+        for (box, digit) in zip(cellRow, boardRow):
+            # unpack the cell coordinates
+            startX, startY, endX, endY = box
+            # compute the coordinates of where the digit will be drawn
+            # on the output puzzle image
+            textX = int((endX - startX) * 0.33)
+            textY = int((endY - startY) * -0.2)
+            textX += startX
+            textY += endY
+            # draw the result digit on the Sudoku puzzle image
+            cv2.putText(puzzleImage, str(digit), (textX, textY),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
+    # show the output image
+    cv2.imshow("Sudoku Result", puzzleImage)
+    cv2.waitKey(0)
